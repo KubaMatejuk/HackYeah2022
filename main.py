576890c5b16938f79b44f7ff4eed8dcca02d8295
Patryk <walter.podsiadlo@gmail.com>
2022-11-19 15:29:22 +0100

    First UI version

--- main.py
+++ main.py
@@ -1,1 +1,32 @@
-# test
+from kivy.uix.screenmanager import ScreenManager, Screen, NoTransition
+from kivy.animation import Animation
+from kivy.properties import StringProperty, ObjectProperty, ListProperty, NumericProperty
+from kivy.clock import Clock
+from kivy.lang.builder import Builder
+
+from kivymd.app import MDApp
+
+
+class WindowManager(ScreenManager):
+    screen_one = ObjectProperty(None)
+    screen_two = ObjectProperty(None)
+
+
+class WasteOverMenu(Screen):
+    pass
+
+
+class WasteOverLogInScreen(Screen):
+    def login(self, **kwargs):
+        password = self.ids.password.text
+        email = self.ids.email.text
+        self.manager.current = 'mainMenu'
+
+
+class WasteOverApp(MDApp):
+    def build(self):
+        m = WindowManager(transition=NoTransition())
+        return m
+
+if __name__ == '__main__':
+    WasteOverApp().run()    
