261f71e24186b8398351bc7d5d9b9a021ad76d8b
pyRafal <70054588+pyRafal@users.noreply.github.com>
2022-11-19 14:11:04 +0100

    Update maps_functions.py
    Added loading markers from db

--- maps_functions.py
+++ maps_functions.py
@@ -1,24 +1,51 @@
 import folium
 import webbrowser
+from db_actions import get_markers, get_category_details
 
-def create_map(center_point, zoom_start=12, width=800, height=400):
-    map = folium.Map(location=center_point, zoom_start=zoom_start, width=width, height=height)
-    return map
+def create_map(center_point=(51.1102, 17.0350), zoom_start=12, width=800, height=600):
+    map_object = folium.Map(location=center_point, zoom_start=zoom_start, width=width, height=height)
+    return map_object
 
 def save_map(map_object, file_name='map.html'):
     map_object.save(file_name)
 
-def open_map(chrome_path, url):
-    webbrowser.get(chrome_path).open(url, new=2)
+def save_marker(map_object, latitude, longitude,
+                marker_name, description,
+                user_name, category_id,
+                photo):
+    category_details = get_category_details(category_id)
+    category_name = category_details['name']
+    marker_colour = category_details['colour']
+    icon = category_details['icon']
 
-def add_new_marker(map_object, latitude, longitude, message, image=None):
     location = float(latitude), float(longitude)
-    folium.Marker(location, popup=message).add_to(map_object)
+    icon = folium.Icon(icon=icon, color=marker_colour)
+
+    html_message =f"""
+        <h1> {marker_name}</h1>
+        <p>Category: {category_name}</p>
+        <p>{description}</p>
+        <img src='{photo}' width=200px>
+        </p>
+        <p>Added by: {user_name}</p>
+        """
+    folium.Marker(location, popup=html_message, icon=icon, tooltip=marker_name).add_to(map_object)
     return map_object
 
-# example flow and data
-map = create_map((51.1102, 17.0350))
-map_with_marker = add_new_marker(map, '51.1174', '16.9962', 'New marker added!')
-save_map(map_with_marker)
+def load_markers():
+    markers = get_markers()
+    map_object = create_map()
+    for marker in markers:
+        save_marker(map_object, marker['latitude'], marker['longitude'],
+                    marker['marker_name'], marker['description'],
+                    marker['username'], marker['category_id'], marker['photo'])
+    save_map(map_object)
+
+# testing purposes
+def open_map(chrome_path, url):
+    webbrowser.get(chrome_path).open(url, new=2)
+load_markers()
 open_map('C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s',
-         'file:C:/Users/Rafał/Desktop//Python/map.html')
+         'file:C:/Users/Rafał/Documents/GitHub/HackYeah2022/map.html')
+
+
