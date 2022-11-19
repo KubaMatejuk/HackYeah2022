c24a8066c1f125a4e1ab07b2a90c96a5e7718942
Patryk <walter.podsiadlo@gmail.com>
2022-11-20 00:10:21 +0100

    LogIn Screen Updated
    - Reorganized elements
    - Added animations
    - QoL changes (added support for tab/enter keys)

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from kivy.uix.screenmanager import ScreenManager, Screen, NoTransition
 from kivy.properties import ObjectProperty
+from kivy.core.window import Window
+from kivy.animation import Animation
+from kivy.clock import Clock
 from db_actions import get_password_for_user
 
 from kivymd.app import MDApp
@@ -21,15 +24,30 @@ class WasteOverMap(Screen):
 
 
 class WasteOverLogInScreen(Screen):
+    def on_enter(self):
+        Clock.schedule_once(self.updating, 1)
+
+    def updating(self, *args):
+        anim_hide = Animation(opacity=1, duration=1)
+        anim_hide += Animation(opacity=0, duration=1)
+        anim_hide.start(self.ids.start_text)
+        anim_show = Animation(opacity=0, duration=2)
+        anim_show += Animation(opacity=1, duration=1)
+        anim_show.start(self.ids.end_text)
+
+
     def login(self, **kwargs):
         password = self.ids.password.text
         email = self.ids.email.text
-        actual_password = get_password_for_user(email)
-        #if password == actual_password:
+        # if password and email:
+            # actual_password = get_password_for_user(email)
+            #if password == actual_password:
         self.manager.current = 'mainMenu'
-
+        
 
 class WasteOverApp(MDApp):
+    Window.size = (450, 700)
+
     def build(self):
         m = WindowManager(transition=NoTransition())
         return m
