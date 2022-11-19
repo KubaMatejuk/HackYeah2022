8bd223990ab1893d975e49af4778b8c52f08fd7a
pyRafal <70054588+pyRafal@users.noreply.github.com>
2022-11-19 17:31:02 +0100

    Update maps_functions.py
    Updates to maps functions

--- maps_functions.py
+++ maps_functions.py
@@ -1,6 +1,6 @@
 import folium
 import webbrowser
-from db_actions import get_markers, get_category_details, add_marker_to_db
+from db_actions import get_markers, get_category, add_marker_to_db, edit_marker_in_db, remove_marker_from_db
 
 def create_map(center_point=(51.1102, 17.0350), zoom_start=12, width=800, height=600):
     map_object = folium.Map(location=center_point, zoom_start=zoom_start, width=width, height=height)
@@ -10,34 +10,27 @@ def save_map(map_object, file_name='map.html'):
     map_object.save(file_name)
 
 def add_market_to_map(map_object, marker_object):
-    # retrieve data from dictionary
-    latitude = marker_object.latitude
-    longitude = marker_object.longitude
-    marker_name = marker_object.marker_name
-    description = marker_object.description
-    user_name = marker_object.user_name
-    category_id = marker_object.category_id
-    photo = marker_object.photo
+    category_object = get_category(marker_object.category_id)
 
-    # retrieve category details
-    category_details = get_category_details(category_id)
-    category_name = category_details['name']
-    marker_colour = category_details['colour']
-    icon = category_details['icon']
-
-    location = float(latitude), float(longitude)
-    icon = folium.Icon(icon=icon, color=marker_colour)
+    icon = folium.Icon(icon=category_object.icon, color=category_object.colour)
 
     html_message =f"""
-        <h1> {marker_name}</h1>
-        <p>Category: {category_name}</p>
-        <p>{description}</p>
-        <img src='{photo}' width=200px>
+        <h1> {marker_object.marker_name}</h1>
+        <p>Category: {category_object.name}</p>
+        <p>{marker_object.description}</p>
+        <img src='{marker_object.photo}' width=200px>
         </p>
-        <p>Added by: {user_name}</p>
+        <p>Added by: {marker_object.user_name}</p>
         """
-    folium.Marker(location, popup=html_message, icon=icon, tooltip=marker_name).add_to(map_object)
+
+    folium.Marker((marker_object.latitude, marker_object.longitude), popup=html_message, icon=icon,
+                  tooltip=marker_object.marker_name).add_to(map_object)
     return map_object
+
+def edit_existing_marker(marker_object):
+    edit_marker_in_db(marker_object)
+    # generate refreshed map
+    load_markers()
 
 def load_markers(user_id=None, categories_id: list = None):
     markers = get_markers(user_id, categories_id)
@@ -45,13 +38,25 @@ def load_markers(user_id=None, categories_id: list = None):
     for marker in markers:
         add_market_to_map(map_object, marker)
     save_map(map_object)
+    return map_object
 
-def add_new_marker(map_object, marker_dict):
+def add_new_marker(map_object, marker_obj):
     try:
-        add_marker_to_db(marker_dict)
-        add_market_to_map(map_object, marker_dict)
+        add_marker_to_db(marker_obj)
+        add_market_to_map(map_object, marker_obj)
     except Exception:
         raise Exception('Adding new marker failed')
+
+def remove_marker(marker_object):
+    remove_marker_from_db(marker_object)
+    # generate refreshed map
+    load_markers()
+
+def get_coordinates(map_object):
+    latitude = map_object['last_clicked']['lat']
+    longitude = map_object['last_clicked']['lng']
+    return latitude, longitude
+
 
 
 
@@ -62,5 +67,6 @@ def open_map(chrome_path, url):
 load_markers()
 open_map('C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s',
          'file:C:/Users/Rafał/Documents/GitHub/HackYeah2022/map.html')
+
 
 
