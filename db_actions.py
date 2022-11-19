4d6615ea4fb6cb44c2ff4a2924f3a1d07d9b70dc
Kuba Matejuk <jmatejuk@gmail.com>
2022-11-19 21:52:17 +0100

    optimizing DB calls

--- db_actions.py
+++ db_actions.py
@@ -4,6 +4,7 @@ from class_marker import Marker
 from class_user import User
 
 
+# DB GENERIC FUNCTIONS
 def db_connect():
     mydb = mysql.connector.connect(
         host="sql7.freesqldatabase.com",
@@ -22,9 +23,12 @@ def run_sql_select(connection, query: str) -> list:
 
 def run_sql_command(connection, query: str) -> list:
     mycursor = connection.cursor()
-    tesr = mycursor.execute(query)
+    mycursor.execute(query)
     connection.commit()
     return mycursor.lastrowid
+
+
+# MARKERS
 
 
 def get_markers_tuple(user_id=None, categories_id: list = None) -> list:
@@ -50,14 +54,69 @@ def get_markers_tuple(user_id=None, categories_id: list = None) -> list:
     return result_tuples_list
 
 
+def add_marker_to_db(marker):
+    new_marker_id = None
+    sql_command = "INSERT INTO `markers` " \
+                  "(`marker_id`, `user_id`, `marker_name`, `description`, " \
+                  "`marker_category_id`, `latitude`, `longitude`) " \
+                  "VALUES " \
+                  "(NULL, '{0}', '{1}', '{2}', '{3}', '{4}', '{5}');".format(
+                    marker.user_id,
+                    marker.marker_name,
+                    marker.description,
+                    marker.category_id,
+                    marker.latitude,
+                    marker.longitude)
+    try:
+        mydb = db_connect()
+        new_marker_id = run_sql_command(mydb, sql_command)
+    except Exception as e:
+        print('Issue when adding marker: {0} to DB: {1}'.format(marker.marker_name, e))
+        pass
+    # add photo
+    return new_marker_id
+
+
+def edit_marker_in_db(marker: Marker):
+    if marker.id:
+        sql_command = "UPDATE `markers` SET " \
+                      "`marker_name` = '{0}', `description` = '{1}', `marker_category_id` = '{2}' " \
+                      "WHERE `markers`.`marker_id` = {3};".format(marker.marker_name, marker.description,
+                                                                  marker.category_id,
+                                                                  marker.id)
+        try:
+            mydb = db_connect()
+            run_sql_command(mydb, sql_command)
+        except Exception as e:
+            print('Issue when editing marker: marker ID: {0} | Error: {1}'.format(marker.id, e))
+            pass
+    else:
+        print("Missing Marker ID")
+        # raise Exception("Missing Marker ID")
+    pass
+
+
+def remove_marker_from_db(marker_id: int):
+    sql_command = "DELETE FROM `markers` WHERE `markers`.`marker_id` = {0}".format(marker_id)
+    try:
+        mydb = db_connect()
+        run_sql_command(mydb, sql_command)
+    except Exception as e:
+        print('Issue when removing marker: marker ID: {0} | Error: {1}'.format(marker_id, e))
+        pass
+    pass
+
+
+# USERS
+
 def get_user_tuple(user_id: int) -> tuple:
     query = "SELECT * FROM users WHERE user_id = {0}".format(user_id)
     return run_sql_select(db_connect(), query)[0]
 
 
-def get_category_tuple(category_id: int) -> dict:
-    query = "SELECT * FROM marker_category WHERE category_id = {0}".format(category_id)
-    return run_sql_select(db_connect(), query)[0]
+def get_users_data() -> list:
+    query = "SELECT user_id, username, email FROM users"
+    return run_sql_select(db_connect(), query)
 
 
 def get_password_for_user(user: str):
@@ -85,7 +144,7 @@ def add_user_to_db(username, email, password):
                   "(`user_id`, `username`, `password`, `email`) " \
                   "VALUES " \
                   "(NULL, '{0}', '{1}', '{2}');".format(
-        username, password, email)
+                    username, password, email)
     try:
         mydb = db_connect()
         new_user_id = run_sql_command(mydb, sql_command)
@@ -94,6 +153,7 @@ def add_user_to_db(username, email, password):
         pass
     # add photo
     return new_user_id
+
 
 def remove_user_from_db(user_id):
     sql_command = "DELETE FROM `users` WHERE `users`.`user_id` = {0}".format(user_id)
@@ -106,70 +166,22 @@ def remove_user_from_db(user_id):
     pass
 
 
-def add_marker_to_db(marker):
-    new_marker_id = None
-    sql_command = "INSERT INTO `markers` " \
-                  "(`marker_id`, `user_id`, `marker_name`, `description`, `marker_category_id`, `latitude`, `longitude`) " \
-                  "VALUES " \
-                  "(NULL, '{0}', '{1}', '{2}', '{3}', '{4}', '{5}');".format(
-        marker.user_id,
-        marker.marker_name,
-        marker.description,
-        marker.category_id,
-        marker.latitude,
-        marker.longitude)
-    try:
-        mydb = db_connect()
-        new_marker_id = run_sql_command(mydb, sql_command)
-    except Exception as e:
-        print('Issue when adding marker: {0} to DB: {1}'.format(marker.marker_name, e))
-        pass
-    # add photo
-    return new_marker_id
+# CATEGORIES
+
+def get_category_tuple(category_id: int) -> dict:
+    query = "SELECT * FROM marker_category WHERE category_id = {0}".format(category_id)
+    return run_sql_select(db_connect(), query)[0]
 
 
-def edit_marker_in_db(marker: Marker):
-    if marker.id:
-        sql_command = "UPDATE `markers` SET " \
-                "`marker_name` = '{0}', `description` = '{1}', `marker_category_id` = '{2}' " \
-                "WHERE `markers`.`marker_id` = {3};".format(marker.marker_name, marker.description, marker.category_id,
-                                                            marker.id)
-        try:
-            mydb = db_connect()
-            run_sql_command(mydb, sql_command)
-        except Exception as e:
-            print('Issue when editing marker: marker ID: {0} | Error: {1}'.format(marker.id, e))
-            pass
-    else:
-        print("Missing Marker ID")
-        # raise Exception("Missing Marker ID")
-    pass
+def get_categories_data() -> list:
+    query = "SELECT * FROM marker_category"
+    return run_sql_select(db_connect(), query)
 
-
-def remove_marker_from_db(marker_id: int):
-    sql_command = "DELETE FROM `markers` WHERE `markers`.`marker_id` = {0}".format(marker_id)
-    try:
-        mydb = db_connect()
-        run_sql_command(mydb, sql_command)
-    except Exception as e:
-        print('Issue when removing marker: marker ID: {0} | Error: {1}'.format(marker_id, e))
-        pass
-    pass
-
-
-test_user = User(user_id=3, username='kubam')
-test_marker = Marker(marker_name='Laptop Applfsdfsfsffsdfe', description='2 letni laptop',
-                     category_id=3, photo=None, latitude=51.1270, longitude=17.1669,
-                     user=test_user)
-
-test_marker2 = Marker(marker_id=19, marker_name='Laptop Apple', description='Roczny laptop',
-                     category_id=3, photo=None, latitude=51.1270, longitude=17.1669,
-                     user=test_user)
-
-# edit_marker_in_db(test_marker2)
-
-# print(add_marker_to_db(test_marker))
-remove_marker_from_db(20)
-
-# for marker in get_markers():
-#     print(marker)
+# test_user = User(user_id=3, username='kubam')
+# test_marker = Marker(marker_name='Laptop Applfsdfsfsffsdfe', description='2 letni laptop',
+#                      category_id=3, photo=None, latitude=51.1270, longitude=17.1669,
+#                      user=test_user)
+#
+# test_marker2 = Marker(marker_id=19, marker_name='Laptop Apple', description='Roczny laptop',
+#                      category_id=3, photo=None, latitude=51.1270, longitude=17.1669,
+#                      user=test_user)
--- object_handler.py
+++ object_handler.py
@@ -1,20 +1,45 @@
 from class_marker import Marker
 from class_marker_category import MarkerCategory
 from class_user import User
-from db_actions import get_category_tuple, get_user_tuple, get_markers_tuple
+from db_actions import get_markers_tuple, add_user_to_db, get_categories_data, get_users_data
+
+CATEGORIES_CACHE = []
+USERS_CACHE = []
 
 
 def get_category(category_id: int) -> MarkerCategory:
-    category_details_tuple = get_category_tuple(category_id)
-    category = MarkerCategory(category_id=category_id, name=category_details_tuple[1],
-                              colour=category_details_tuple[3], description=category_details_tuple[2],
-                              icon=category_details_tuple[4])
+    category_details_tuple = None
+    global CATEGORIES_CACHE
+    if not CATEGORIES_CACHE:
+        CATEGORIES_CACHE = get_categories_data()
+    for category_tuple in CATEGORIES_CACHE:
+        if category_tuple[0] == category_id:
+            category_details_tuple = category_tuple
+            break
+    if category_details_tuple:
+        category = MarkerCategory(category_id=category_id, name=category_details_tuple[1],
+                                  colour=category_details_tuple[3], description=category_details_tuple[2],
+                                  icon=category_details_tuple[4])
+    else:
+        category = None
     return category
 
 
 def get_user(user_id: int) -> User:
-    user_tuple = get_user_tuple(user_id)
-    user = User(user_id=user_id, username=user_tuple[1])
+    username = None
+    email = None
+    global USERS_CACHE
+    if not USERS_CACHE:
+        USERS_CACHE = get_users_data()
+    for user_tuple in USERS_CACHE:
+        if user_tuple[0] == user_id:
+            username = user_tuple[1]
+            email = user_tuple[2]
+            break
+    if username:
+        user = User(user_id=user_id, username=username, email=email)
+    else:
+        user = None
     return user
 
 
@@ -26,3 +51,10 @@ def get_markers(user_id=None, categories_id: list = None) -> list:
                         user=get_user(marker_tuple[1]))
         markers_list.append(marker)
     return markers_list
+
+
+def add_user(username: str, email: str, password: str) -> User:
+    user_id = add_user_to_db(username, email, password)
+    user = User(user_id=user_id, username=username, password=password, email=email)
+    return user
+
