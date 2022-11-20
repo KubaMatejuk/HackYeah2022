77fad58aedcd35523139c56e6760f25789548fdb
pyRafal <70054588+pyRafal@users.noreply.github.com>
2022-11-20 01:13:09 +0100

    Update main.py
    Unfinished changes to loading markers

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 from kivy.uix.screenmanager import ScreenManager, Screen, NoTransition
+from kivy.uix.label import Label
+from kivy.uix.popup import Popup
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.textinput import TextInput
 from kivy.properties import ObjectProperty
 from kivy.core.window import Window
 from kivy.animation import Animation
 from kivy.clock import Clock
 from db_actions import get_password_for_user
 
+from object_handler import get_markers
 from kivymd.app import MDApp
+from kivy_garden.mapview import MapMarkerPopup
 
 
 class WindowManager(ScreenManager):
@@ -29,6 +35,51 @@ class WasteOverMenu(Screen):
 
 class WasteOverMap(Screen):
     map = ObjectProperty(None)
+    placed = False
+    exists = False
+    marker = False
+
+    def place_pin(self):
+        self.placed = True
+        pass
+
+    def remove_pin(self):
+        if self.exists:
+            self.map.remove_widget(self.marker)
+            self.placed = False
+            self.exists = False
+        pass
+
+    def on_touch_up(self, touch):
+        if touch.y > self.height * 0.05:
+            if self.placed == True and self.exists == False:
+                self.marker = MapMarkerPopup(lat=self.map.get_latlon_at(touch.x, touch.y)[0],
+                                             lon=self.map.get_latlon_at(touch.x, touch.y)[1])
+                self.marker.add_widget(TextInput(text="TEST"))
+                self.map.add_widget(self.marker)
+                self.exists = True
+                print(self.map.get_latlon_at(touch.x, touch.y))
+
+    def load_markers(self):
+        markers_list = get_markers()
+        for marker in markers_list:
+            self.add_marker_to_map(marker)
+
+    def add_marker_to_map(self, marker):
+        marker_widget = MapMarkerPopup(lat=marker.latitude, lon=marker.longitude)
+        # marker_widget.add_widget(Label(text=marker.marker_name + '\n'
+        #                                + str(marker.description) + '\n'
+        #                                + 'Added by: ' + marker.user_name, color= [0, 0, 0]))
+        marker_widget.add_widget(Popup(title=marker.marker_name,
+                                       content=Label(text=marker.marker_name + '\n'
+                                                          + str(marker.description) + '\n'
+                                                          + 'Added by: ' + marker.user_name),
+                                       size_hint=(None, None), size=(200, 200)))
+
+        self.map.add_widget(marker_widget)
+
+    def on_enter(self, *args):
+        self.load_markers()
 
 class WasteOverBarReader(Screen):
     bat = ObjectProperty(None)
